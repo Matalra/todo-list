0d5758aced5bc6ae5621c436a9dd6dffc1d99b61
Matalra <main_matheusferreira@hotmail.com>
2021-09-05 21:27:07 -0300

    add general style and base Js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+const inputTarefa = document.querySelector('.input-tarefa')
+const addTarefa = document.querySelector('.adicionar')
+const tarefas = document.querySelector('.tarefas')
+
+addTarefa.addEventListener('click', function(){
+    if(!inputTarefa.value){
+        console.log('vazio');
+        return
+    }
+    console.log(inputTarefa.value);
+})
